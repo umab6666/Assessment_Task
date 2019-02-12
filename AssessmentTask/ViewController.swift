d996ae3980f1f5d98ca1cb6bce1cc0bbed599e24
uma@thisisswitch.in <uma@thisisswitch.in>
2019-02-12 13:30:14 +0530

    Initial Commit

---
+++ AssessmentTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssessmentTask
+//
+//  Created by Uma B on 12/02/19.
+//  Copyright © 2019 Uma B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
